fed152f20a57d8bf1f434e1889939a0cdedef916
gustavorteuber <gugareis135@gmail.com>
2023-01-28 13:14:36 -0300

    pronto-para-deploy

--- ulysses/settings.py
+++ ulysses/settings.py
@@ -12,17 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-gm@rmmd3xt==h5%*2zsikash#94g0s59!*1mv$=$o^!w0x8_t_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
