cf34bbf22317cff713ff32ecf6614f6c6907521e
gustavorteuber <gugareis135@gmail.com>
2023-01-30 18:19:22 -0300

    postgres

--- ulysses/settings.py
+++ ulysses/settings.py
@@ -82,8 +82,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoshallusa',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
